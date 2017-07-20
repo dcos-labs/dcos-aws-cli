4996f1f9c31baf227ab6454cfed8c5e4f0ccc508
Joerg Schad <joerg@mesosphere.io>
2017-07-19 17:44:09 -0700

    Improve formatting.

--- src/cli.go
+++ src/cli.go
@@ -16,8 +16,6 @@ import (
 func main() {
 	app := cli.New()
 
-	// cli.HandleDefaultSections(app)
-
 	handlePublicIPs(app)
 
 	kingpin.MustParse(app.Parse(cli.GetArguments()))
@@ -35,12 +33,6 @@ func runDcosCommand(arg ...string) {
 		fmt.Printf("Unable to run DC/OS command: %s\n", strings.Join(arg, " "))
 		fmt.Printf("Make sure your PATH includes the 'dcos' executable.\n")
 	}
-
-	// nodesJson := out.String()
-
-
-
-
 }
 
 func publicAgentIPs(c *kingpin.ParseContext) error {
@@ -69,38 +61,16 @@ func publicAgentIPs(c *kingpin.ParseContext) error {
 		sshExec := exec.Command(dcosCommand, "node", "ssh", "--option", "StrictHostKeyChecking=no", 
 		 "--option", "LogLevel=quiet", "--master-proxy", "--mesos-id="+value.String(), `"curl -s ifconfig.co"`)
 		sshExec.Stdout = os.Stdout
-		//sshExec.Stderr = os.Stderr
 		sshExec.Stdin = os.Stdin
 
 		err := sshExec.Run()
 		if err != nil {
 			fmt.Printf("[Error during ssh to nodes.] %s\n\n", err)
 			fmt.Printf("Make sure ssh keys are configured correctly.\n")
-
 		}
-
 		return true // keep iterating
-})
-
-
-
-	// m := f.(map[string]interface{})
-	// for k, v := range m {
- //    switch vv := v.(type) {
- //    case string:
- //        fmt.Println(k, "is string", vv)
- //    case int:
- //        fmt.Println(k, "is int", vv)
- //    case []interface{}:
- //        fmt.Println(k, "is an array:")
- //        for i, u := range vv {
- //            fmt.Println(i, u)
- //        }
- //    default:
- //        fmt.Println(k, "is of a type I don't know how to handle")
- //    }
- //  }
-
+	})
+	
 	return nil
 }
 

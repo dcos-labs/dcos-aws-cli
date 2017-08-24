b0e3e3ff8c6fc97070f7d4a4c0c8fb23d18636d3
Joerg Schad <joerg@mesosphere.io>
2017-08-23 21:15:06 -0700

    Added sha computation to build script.

--- tools/build_cli.sh
+++ tools/build_cli.sh
@@ -16,5 +16,8 @@ cd ../src
 go get
 
 CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -ldflags="-s -w" -o $CLI_EXE_NAME".exe"
+echo  $(shasum -a 256 $CLI_EXE_NAME.exe)
 CGO_ENABLED=0 GOOS=darwin GOARCH=386 go build -ldflags="-s -w" -o $CLI_EXE_NAME"-darwin"
+echo $(shasum -a 256 $CLI_EXE_NAME-darwin)
 CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -ldflags="-s -w" -o $CLI_EXE_NAME"-linux"
+echo $(shasum -a 256 $CLI_EXE_NAME-linux)
